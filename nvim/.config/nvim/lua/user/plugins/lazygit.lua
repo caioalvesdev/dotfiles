3aaa8cd22f027e4d05f1ce35c4b41da8e58931d7
caio-alves <ktinserx2@gmail.com>
2024-10-11 15:00:21 -0300

    chore: add lazygit
    Adicionando LazyGit

---
+++ nvim/.config/nvim/lua/user/plugins/lazygit.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>gg', ':LazyGit<CR>', { noremap = true, silent = true })
+    end
+  }
+}
